e47b8c1f804eb2210df0cfdb4fc8e705499d2bcd
AntonioChirik <74306979+antoniochirik@users.noreply.github.com>
2021-03-31 15:10:31 +0300

    chenge test-2 (#2)
    Co-authored-by: antoniochirik <antoniochirik@yandex.ru>

--- ne_vodka_project.py
+++ ne_vodka_project.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-vodka = 1.0
-rain = 0.0
-friend = 1.0
+vodka = 0.0
+rain = 1.0
+friend = 0.0
 
 
 def activation_fanction(x):
-    if x >= 0.5:
+    if x >= 0.6:
         return 1
     else:
         return 0
@@ -22,13 +22,13 @@ def predict(vodka, rain, friend):
     weight_input_to_output = ([-1, 1])
 
     hidden_input = np.dot(weight_input_to_hidden, inputs)
-    print("hidden_input: " + str(hidden_input))
+    print(str(hidden_input))
 
     hidden_output = np.array([activation_fanction(x) for x in hidden_input])
-    print('hidden_output: ' + str(hidden_output))
+    print(str(hidden_output))
 
     output = np.dot(weight_input_to_output, hidden_output)
-    print("output: " + str(output))
+    print(str(output))
     return activation_fanction(output) == 1
 
 
